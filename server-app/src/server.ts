bf3c4f83287d65dab4bec44644c29ae49d462e72
vidhisingh151 <vidhisinghrana24@gmail.com>
2026-02-01 20:13:58 +0530

    Updated cors

--- server-app/src/server.ts
+++ server-app/src/server.ts
@@ -19,7 +19,9 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin:[ "http://localhost:3000", 
+          "https://loquacious-hummingbird-d344fe.netlify.app" ],
+
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
